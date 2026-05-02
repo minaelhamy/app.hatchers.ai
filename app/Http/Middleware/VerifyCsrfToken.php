482b8773cb71f4255c1d198d71f1cf43483a35e0
Mina <minaelhamy@gmail.com>
2026-05-02 16:07:19 +0300

    The screenshots helped a lot. The date click is working, but the selected date wasn’t turning into time slots because the proxied AJAX calls were getting blocked by Hatchers with 419.
    I patched VerifyCsrfToken.php to allow the proxied booking endpoints through:
    
    */service/timeslot
    */service/slotlimit
    */service/stafflimit
    */service/booking
    That’s the right layer here because the booking page is rendered from Servio, but the requests first hit app.hatchers.ai’s proxy route.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -7,5 +7,9 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 class VerifyCsrfToken extends Middleware
 {
     protected $except = [
+        '*/service/timeslot',
+        '*/service/slotlimit',
+        '*/service/stafflimit',
+        '*/service/booking',
     ];
 }
